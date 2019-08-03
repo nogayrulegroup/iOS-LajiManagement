b16c17ab67eb33adad66f390b9130a0481560ffc
ruanjiewei <ruanjiewei@huya.com>
2019-08-03 11:24:38 +0800

    Initial Commit

---
+++ LajiDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LajiDemo
+//
+//  Created by huya on 2019/8/3.
+//  Copyright © 2019 nogayrulegroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
